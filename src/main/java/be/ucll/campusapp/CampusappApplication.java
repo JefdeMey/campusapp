836e48873709b4f05cd5edfe6992cfc34499eff4
Jef de Mey <r1009803@ucll.be>
2025-05-15 10:58:19 +0200

    Eerste commit - project opstart

---
+++ src/main/java/be/ucll/campusapp/CampusappApplication.java
@@ -0,0 +1,13 @@
+package be.ucll.campusapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampusappApplication.class, args);
+	}
+
+}
